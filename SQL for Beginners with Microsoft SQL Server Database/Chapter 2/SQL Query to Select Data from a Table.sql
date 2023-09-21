f297fcd9408bb9714dd921b6fd5c9783a966be94
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-09-21 15:01:11 -0400

    Initial Commit

---
+++ SQL for Beginners with Microsoft SQL Server Database/Chapter 2/SQL Query to Select Data from a Table.sql
@@ -0,0 +1,14 @@
+USE AdventureWorks2017;
+GO
+
+SELECT FirstName
+FROM [Person].[Person];
+GO
+
+SELECT FirstName, MiddleName, LastName
+FROM [Person].[Person];
+GO
+
+SELECT * --ALL Columns from Person.Person Table
+FROM [Person].[Person];
+GO
---
+++ SQL for Beginners with Microsoft SQL Server Database/Chapter 2/Selecting Top N records.sql
@@ -0,0 +1,17 @@
+USE AdventureWorks2017;
+GO
+
+--SELECT TOP NUMBER
+-- FROM Table
+
+--SELECT TOP PERCENT%
+--FROM Table
+
+SELECT *
+FROM [Person].[Person];
+
+SELECT TOP 5 *
+FROM [Person].[Person];
+
+SELECT TOP 5 Percent *
+FROM [Person].[Person];
